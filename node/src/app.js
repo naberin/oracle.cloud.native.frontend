d7f766e663c35be3b785921a3a971adbd867d09c
naberin <aberinnj@gmail.com>
2021-09-15 23:58:44 -0500

    Added Initial VueJS frontend files

--- node/src/app.js
+++ node/src/app.js
@@ -4,8 +4,12 @@ const port = process.env.port ? process.env.port : 3000;
 
 const healthRoutes = require('./routes/health');
 
+// const blogRoutes = require('');
+// const newsRoutes = require('');
 
 app.use('/health', healthRoutes);
+// app.use('/api/blogs', blogRoutes);
+// app.use('/api/news', newsRoutes);
 
 app.listen(port, () => {
     console.log(`Application running at http://localhost:${port}`)

544892916f2cb520e7c58c9c12486c73d4c1e188
naberin <aberinnj@gmail.com>
2021-09-22 23:06:28 -0500

    Updated Header component with title

--- portal/src/components/Header.vue
+++ portal/src/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-  <header></header>
+  <header class="app-header">
+    <div class="container flex flex-row justify-between">
+      <div class="title">
+        <h4>Scoreboard</h4>
+      </div>
+      <div class="">
+
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,5 +18,15 @@ export default {
 </script>
 
 <style scoped>
+  .app-header {
+    height: 60px;
+    width: 100%;
+    background: #292929;
+  }
 
+  .app-header .title {
+    padding-left: 9px;
+    padding-right: 9px;
+    color: #fff;
+  }
 </style>
